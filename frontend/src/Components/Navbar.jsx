d07e00cee49f0312039593a0bb507114b5bb77e3
emmanyouwell <emingala02@gmail.com>
2025-01-15 12:55:11 +0800

    added donor screening form and landing page

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import {
   Navbar,
   MobileNav,
@@ -7,17 +7,17 @@ import {
   IconButton,
   Card,
 } from "@material-tailwind/react";
- 
+import { Link } from "react-router-dom";
 const StickyNavbar = () => {
   const [openNav, setOpenNav] = React.useState(false);
- 
+
   useEffect(() => {
     window.addEventListener(
       "resize",
       () => window.innerWidth >= 960 && setOpenNav(false),
     );
   }, []);
- 
+
   const navList = (
     <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
       <Typography
@@ -27,10 +27,10 @@ const StickyNavbar = () => {
         className="p-1 font-normal font-varela"
       >
         <a href="#" className="flex items-center">
-          Pages
+          Articles
         </a>
       </Typography>
-      <Typography
+      {/* <Typography
         as="li"
         variant="small"
         color="white"
@@ -59,81 +59,77 @@ const StickyNavbar = () => {
         <a href="#" className="flex items-center">
           Docs
         </a>
-      </Typography>
+      </Typography> */}
     </ul>
   );
- 
+
   return (
-    
-      <Navbar className="sticky top-3 z-20 h-max w-[50%] mx-auto rounded-3xl px-4 py-2 lg:px-8 lg:py-4 bg-[#E53777]">
-        <div className="flex items-center justify-between text-light">
-          <Typography
-            as="a"
-            href="#"
-            className="mr-4 cursor-pointer py-1.5 font-bold font-varela"
-          >
-            TCHMB Portal
-          </Typography>
-          <div className="flex items-center gap-4">
-            <div className="mr-4 hidden lg:block">{navList}</div>
-            {/* <div className="flex items-center gap-x-1">
-              <Button
-                variant="text"
-                size="sm"
-                className="hidden lg:inline-block"
-              >
-                <span>Log In</span>
-              </Button>
-              <Button
-                variant="gradient"
-                size="sm"
-                className="hidden lg:inline-block"
-              >
-                <span>Sign in</span>
-              </Button>
-            </div> */}
-            <IconButton
-              variant="text"
-              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-              ripple={false}
-              onClick={() => setOpenNav(!openNav)}
+
+    <Navbar className="sticky top-3 z-20 h-max w-[50%] mx-auto rounded-3xl px-4 py-2 lg:px-8 lg:py-4 bg-[#E53777]">
+      <div className="flex items-center justify-between text-light">
+        <Typography
+          as="a"
+          href="#"
+          className="mr-4 cursor-pointer py-1.5 font-bold font-varela"
+        >
+          TCHMB Portal
+        </Typography>
+        <div className="flex items-center gap-4">
+          <div className="mr-4 hidden lg:block">{navList}</div>
+          <div className="flex items-center gap-x-1">
+            <Link to="/login">
+            <Button
+              variant="gradient"
+              size="sm"
+              color="white"
+              className="hidden lg:inline-block"
             >
-              {openNav ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  className="h-6 w-6"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              )}
-            </IconButton>
+              <span>Log In</span>
+            </Button>
+            </Link>
           </div>
+          <IconButton
+            variant="text"
+            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+            ripple={false}
+            onClick={() => setOpenNav(!openNav)}
+          >
+            {openNav ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                className="h-6 w-6"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            )}
+          </IconButton>
         </div>
-        <MobileNav open={openNav}>
-          {navList}
-          {/* <div className="flex items-center gap-x-1">
+      </div>
+      <MobileNav open={openNav}>
+        {navList}
+        {/* <div className="flex items-center gap-x-1">
             <Button fullWidth variant="text" size="sm" className="">
               <span>Log In</span>
             </Button>
@@ -141,10 +137,10 @@ const StickyNavbar = () => {
               <span>Sign in</span>
             </Button>
           </div> */}
-        </MobileNav>
-      </Navbar>
-     
-    
+      </MobileNav>
+    </Navbar>
+
+
   );
 }
 
