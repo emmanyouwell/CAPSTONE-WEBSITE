873216d1fc65ccf8ca0df79a0a718487f0737957
emmanyouwell <emingala02@gmail.com>
2025-05-15 08:35:06 +0800

    added charts

--- frontend/src/Pages/DashboardLayout.jsx
+++ frontend/src/Pages/DashboardLayout.jsx
@@ -20,21 +20,18 @@ const DashboardLayout = () => {
     const [isNavOpen, setIsNavOpen] = useState(false)
     return (
         <>
-        {/* <TemplateScriptsLoader/> */}
-        <div className="flex w-[100%] flex-1 items-stretch">
-            <div className="min-h-[100vh] w-[100%] flex items-stretch">
-                <SidebarComponent userDetails={userDetails} />
-                <div className="flex flex-1 items-stretch basis-[100%] flex-col p-0">
-                    <ComplexNavbar setIsNavOpen={setIsNavOpen}/>
-                    <div className="flex flex-1 flex-col items-stretch justify-between">
-                        <div className="flex-grow">
+            {/* <TemplateScriptsLoader/> */}
+            <div className="flex w-[100%] flex-1 items-stretch ">
+                <div className="min-h-[calc(100vh-2rem)] w-[100%] flex items-stretch">
+                    <SidebarComponent userDetails={userDetails} />
+                    <div className="flex flex-1 items-stretch basis-[100%] flex-col p-0">
+                        <ComplexNavbar setIsNavOpen={setIsNavOpen} />
+                        <div className="flex flex-1 flex-col items-stretch justify-between ">
                             <Outlet />
                         </div>
-
                     </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
