c7f02a7385782690f5ab1a4504d33d9fd3df9d23
emmanyouwell <emingala02@gmail.com>
2025-05-17 16:04:11 +0800

    added interview form submissions form

--- frontend/src/Components/Admin/Donors/SingleDonor.jsx
+++ frontend/src/Components/Admin/Donors/SingleDonor.jsx
@@ -69,15 +69,15 @@ const SingleDonor = () => {
 
                                 <div className="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
                                     <dt className="font-medium text-gray-900">Eligibility</dt>
-                                    <dd className="text-gray-700 sm:col-span-2">--</dd>
+                                    <dd className="text-gray-700 sm:col-span-2">{donorDetails ? donorDetails.eligibility : '--'}</dd>
                                 </div>
 
-                                <div className="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
+                                {/* <div className="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
                                     <dt className="font-medium text-gray-900">Date tested:</dt>
                                     <dd className="text-gray-700 sm:col-span-2">
                                         --
                                     </dd>
-                                </div>
+                                </div> */}
                             </dl>
                         </div>
                     </div>
---
+++ frontend/src/Pages/Admin/Eligibility/Submissions.jsx
@@ -0,0 +1,143 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getSubmissions, updateDonor } from '../../../redux/actions/donorActions';
+import { Button, Card, Dialog, DialogBody, DialogFooter, DialogHeader, IconButton, Input, Typography } from '@material-tailwind/react';
+import { Link } from 'react-router-dom';
+import { CheckCheck, EyeIcon } from 'lucide-react';
+import { formatDate } from '../../../utils/helper';
+import { XMarkIcon } from '@heroicons/react/24/outline';
+import { toast } from 'react-toastify';
+import { resetUpdate } from '../../../redux/slices/donorSlice';
+
+
+const Submissions = () => {
+    const dispatch = useDispatch();
+    const { submissions, isUpdated, loading } = useSelector((state) => state.donors);
+    useEffect(() => {
+        dispatch(getSubmissions());
+        if (isUpdated){
+            dispatch(resetUpdate())
+        }
+    }, [dispatch, isUpdated])
+    const eligibilityOptions = ['Eligible', 'Not Eligible'];
+    const [open, setOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState('');
+    const [id, setId] = useState('');
+    const handleOpen = (id) => {
+        setOpen(!open)
+        setId(id);
+    };
+    const handleChange = (e) => {
+        setSelectedOption(e.target.value)
+    }
+    const submit = () => {
+        console.log("Selected option: ", selectedOption);
+        console.log("ID: ", id);
+        const data = {
+            id: id,
+            eligibility: selectedOption,
+            verified: true
+        }
+        dispatch(updateDonor(data)).then(() => {
+            toast.success("Eligibility updated successfully", { position: "bottom-right" });
+            setOpen(!open);
+        });
+
+    }
+    return (
+        <div className="w-full h-full p-4">
+            <Card className="h-full w-full overflow-scroll">
+                <table className="w-full min-w-max table-auto text-left">
+                    <thead className="bg-secondary text-white">
+                        <tr>
+                            <th className="border-b p-4">Donor Name</th>
+                            <th className="border-b p-4">Eligibility</th>
+                            <th className="border-b p-4">Submission Date</th>
+                            <th className="border-b p-4">Submission ID</th>
+                            <th className="border-b p-4">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {submissions && submissions.map(({ _id, user, lastSubmissionDate, submissionID, eligibility }, index) => (
+                            <tr key={index}>
+                                <td className="p-4">
+                                    {`${user.name.first} ${user.name.last}`}
+                                </td>
+                                <td className="p-4">{eligibility}</td>
+                                <td className="p-4">{formatDate(lastSubmissionDate)}</td>
+                                <td className="p-4">{submissionID}</td>
+                                <td className="p-4 flex items-center gap-2">
+                                    {/* Filtered View */}
+                                    <a target="_blank" href={`https://script.google.com/macros/s/AKfycbzoZwYmUu_dJK-JLOTGgHEhc0p_V-Sh2hPpv0ud4eybG6xER9IVDNbFEtuWH7a9KzyS/exec?submissionId=${submissionID}`}>
+                                        <Button className="bg-secondary"><EyeIcon className="h-5 w-5" /></Button>
+                                    </a>
+
+                                    <Button className="bg-success" onClick={() => handleOpen(_id)}><CheckCheck className="h-5 w-5" /></Button>
+                                </td>
+
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </Card>
+            <Dialog size="sm" open={open} handler={handleOpen} className="p-4">
+                <DialogHeader className="relative m-0 block">
+                    <Typography variant="h4" color="blue-gray">
+                        Enter Eligibility
+                    </Typography>
+                    <Typography className="mt-1 font-normal text-gray-600">
+                        Please select the fridge you would like to store the milk in.
+                    </Typography>
+                    <IconButton
+                        size="sm"
+                        variant="text"
+                        className="!absolute right-3.5 top-3.5"
+                        onClick={handleOpen}
+                    >
+                        <XMarkIcon className="h-4 w-4 stroke-2" />
+                    </IconButton>
+                </DialogHeader>
+                <DialogBody>
+                    <div className="space-y-4">
+
+                        {eligibilityOptions?.length > 0 && eligibilityOptions.map((type, index) => (
+                            <div key={index}>
+                                <input
+                                    type="radio"
+                                    id={type}
+                                    name={type}
+                                    value={type}
+                                    className="peer hidden"
+                                    required
+                                    checked={selectedOption === type}
+                                    onChange={handleChange}
+                                />
+                                <label
+                                    htmlFor={type}
+                                    className="block w-full cursor-pointer rounded-lg border border-gray-300 p-4 text-gray-900 ring-1 ring-transparent peer-checked:border-gray-900 peer-checked:ring-gray-900"
+                                >
+                                    <div className="block">
+                                        <Typography className="font-semibold">
+                                            {type}
+                                        </Typography>
+
+                                    </div>
+                                </label>
+                            </div>
+                        ))}
+
+
+                    </div>
+                </DialogBody>
+                <DialogFooter>
+                    <Button className="ml-auto" onClick={submit}>
+                        Select
+                    </Button>
+                </DialogFooter>
+            </Dialog>
+
+        </div>
+    )
+}
+
+export default Submissions
