a12efa13ecca22fe2c37da011f82d5ce5bd1c15d
emmanyouwell <emingala02@gmail.com>
2025-05-25 22:19:01 +0800

    Fixed Login Bugs

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -9,15 +9,17 @@ import * as Yup from 'yup'
 import { Link, useNavigate } from 'react-router-dom'
 import { Typography, Input, Button } from '@material-tailwind/react'
 import logo from '../assets/image/TCHMB-logo.png'
-
+import Loader from '../Components/Loader/Loader'
 import { DefaultGallery } from '../Components/Gallery'
 import { toast } from 'react-toastify'
 import { resetError } from '../redux/slices/userSlice'
-
+import {Eye, EyeOff} from 'lucide-react'
 const Login = () => {
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const { isLoggedIn, userDetails, loading, error } = useSelector(state => state.users)
+  const [load, setLoad] = useState(true)
+  const [showPassword, setShowPassword] = useState(false);
   const formik = useFormik({
     initialValues: {
       email: "",
@@ -48,24 +50,32 @@ const Login = () => {
       dispatch(resetError());
     }
   }, [isLoggedIn, error])
- 
+  useEffect(() => {
+    // Simulate a delay (e.g. 500ms)
+    const timer = setTimeout(() => {
+      setLoad(false);
+    }, 1000);
+
+    // Cleanup timer on unmount
+    return () => clearTimeout(timer);
+  }, []);
+
+
+
   return (
     <>
       <StickyNavbar />
 
-      <section
-
-        className="bg-white hero flex w-full h-screen items-center justify-center p-4">
-
+      <section className="bg-white hero flex w-full h-screen items-center justify-center p-4">
         <div className="flex flex-col w-full items-center justify-start  h-full">
           <div className="p-4 flex flex-col items-center justify-center ">
             <img src={logo} alt="logo" className="w-40 h-40 lg:w-52 lg:h-52" />
             <p className='font-dm text-3xl text-center text-white'>Welcome to TCHMB Portal!</p>
           </div>
-
-
-          <div className="w-full max-w-md bg-white rounded-lg shadow-md p-6">
-            <div className="w-full">
+          <div className="w-full max-w-md min-h-72 bg-white rounded-lg shadow-md p-6">
+            {load ? (<div className="flex items-center justify-center h-[100%] w-full">
+              <Loader />
+            </div>) : <div className="w-full">
               <h2 className="text-2xl font-bold text-gray-900 mb-4">Login</h2>
               {/* Formik Form */}
               <form className="flex flex-col" onSubmit={formik.handleSubmit}>
@@ -82,20 +92,30 @@ const Login = () => {
                 {formik.touched.email && formik.errors.email && (
                   <p className="text-red-500 text-sm mb-2">{formik.errors.email}</p>
                 )}
+                <div className="relative">
+                  {/* Password Input */}
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    name="password"
+                    placeholder="Password"
+                    className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-2 w-full pr-10 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-secondary transition ease-in-out duration-150"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.password}
+                  />
+                  {/* Toggle Button */}
+                  <button
+                    type="button"
+                    onClick={() => setShowPassword(prev => !prev)}
+                    className="absolute right-2 top-2 text-sm text-gray-600 focus:outline-none hover:text-secondary"
+                  >
+                    {showPassword ? <EyeOff/> : <Eye/>}
+                  </button>
+                  {formik.touched.password && formik.errors.password && (
+                    <p className="text-red-500 text-sm mb-2">{formik.errors.password}</p>
+                  )}
+                </div>
 
-                {/* Password Input */}
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                  className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-2 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-secondary transition ease-in-out duration-150"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.password}
-                />
-                {formik.touched.password && formik.errors.password && (
-                  <p className="text-red-500 text-sm mb-2">{formik.errors.password}</p>
-                )}
 
 
 
@@ -115,14 +135,9 @@ const Login = () => {
                   Login
                 </button>
               </form>
-            </div>
+            </div>}
           </div>
-
-
-
         </div>
-
-
       </section>
 
     </>
