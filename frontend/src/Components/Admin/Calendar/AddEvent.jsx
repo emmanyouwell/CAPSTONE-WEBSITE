f116fdc61e94736c2fec95c40e827750f8ec41e1
emmanyouwell <emingala02@gmail.com>
2025-05-27 21:59:24 +0800

    used drawer for add event

--- frontend/src/Components/Admin/Calendar/AddEvent.jsx
+++ frontend/src/Components/Admin/Calendar/AddEvent.jsx
@@ -11,6 +11,7 @@ import {
     DialogBody,
     DialogHeader,
     DialogFooter,
+    Drawer,
 } from "@material-tailwind/react";
 import { XMarkIcon, PlusIcon } from "@heroicons/react/24/outline";
 import { useFormik } from 'formik';
@@ -23,7 +24,7 @@ import { createLetting, getLettings } from "../../../redux/actions/lettingAction
 export function AddEvent() {
     const dispatch = useDispatch();
     const [open, setOpen] = useState(false);
-    const {success} = useSelector(state => state.lettings);
+    const { success } = useSelector(state => state.lettings);
     const handleOpen = () => setOpen(!open);
     const validationSchema = Yup.object({
         title: Yup.string().required("Title is required"),
@@ -84,151 +85,159 @@ export function AddEvent() {
             dispatch(getLettings());
         }
     }, [success])
+
     return (
         <>
             <Button onClick={handleOpen} className="flex items-center gap-2 bg-secondary" size="sm">
                 <PlusIcon className="h-5 w-5" strokeWidth={2} /> Add Event
             </Button>
-            <Dialog size="sm" open={open} handler={handleOpen} className="p-4">
+            <Drawer open={open} onClose={handleOpen} className="p-4" size={500}>
                 <form onSubmit={formik.handleSubmit}>
-                    <DialogHeader className="relative m-0 block">
-                        <Typography variant="h4" color="blue-gray">
-                            Add a new event
-                        </Typography>
-                        <Typography className="mt-1 font-normal text-gray-600">
-                            Enter the details of the event.
-                        </Typography>
-                        <IconButton
-                            size="sm"
-                            variant="text"
-                            className="!absolute right-3.5 top-3.5"
-                            onClick={handleOpen}
-                        >
-                            <XMarkIcon className="h-4 w-4 stroke-2" />
-                        </IconButton>
-                    </DialogHeader>
-                    <DialogBody className="space-y-4 pb-6">
-                        <div>
-                            <Typography variant="small" color="blue-gray" className="mb-2 font-medium">
-                                Title
+                    <div className="overflow-y-auto flex-1 pr-2 space-y-4">
+                        <div className="relative m-0 block">
+                            <Typography variant="h4" color="blue-gray">
+                                Add a new event
                             </Typography>
-                            <Input
-                                name="title"
-                                placeholder="e.g. Breastfeeding Awareness Month"
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                value={formik.values.title}
-                                error={formik.touched.title && Boolean(formik.errors.title)}
-                            />
-                            {formik.touched.title && formik.errors.title && (
-                                <Typography color="red" variant="small">
-                                    {formik.errors.title}
-                                </Typography>
-                            )}
-                        </div>
-                        <div>
-                            <Typography variant="small" color="blue-gray" className="mb-2 font-medium">
-                                Venue
+                            <Typography className="mt-1 font-normal text-gray-600">
+                                Enter the details of the event.
                             </Typography>
-                            <Input
-                                name="venue"
-                                placeholder="e.g. Taguig City"
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                value={formik.values.venue}
-                                error={formik.touched.venue && Boolean(formik.errors.venue)}
-                            />
-                            {formik.touched.venue && formik.errors.venue && (
-                                <Typography color="red" variant="small">
-                                    {formik.errors.venue}
-                                </Typography>
-                            )}
+                            <IconButton
+                                size="sm"
+                                variant="text"
+                                className="!absolute right-3.5 top-3.5"
+                                onClick={handleOpen}
+                            >
+                                <XMarkIcon className="h-4 w-4 stroke-2" />
+                            </IconButton>
                         </div>
-                        <div>
-                            <Typography variant="small" color="blue-gray" className="mb-2 font-medium">
-                                Description (Optional)
-                            </Typography>
-                            <Textarea
-                                name="description"
-                                placeholder="e.g. Hold a seminar on the importance of breastfeeding."
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                value={formik.values.description}
-                            />
-                        </div>
-                        <div className="flex gap-4">
-                            <div className="w-full">
+                        <div className="space-y-4 pb-6">
+                            <div>
                                 <Typography variant="small" color="blue-gray" className="mb-2 font-medium">
-                                    Status
-                                </Typography>
-                                <Select
-                                    name="status"
-                                    onChange={(val) => formik.setFieldValue("status", val)}
-                                    onBlur={formik.handleBlur}
-                                    value={formik.values.status}
-                                    error={formik.touched.status && Boolean(formik.errors.status)}
-                                >
-                                    <Option value="Not-Due">Not-Due</Option>
-                                    <Option value="On-Going">On-Going</Option>
-                                    
-                                </Select>
-                                {formik.touched.status && formik.errors.status && (
-                                    <Typography color="red" variant="small">
-                                        {formik.errors.status}
-                                    </Typography>
-                                )}
-                            </div>
-
-                        </div>
-
-
-                        <div className="flex gap-4">
-                            <div className="w-full">
-                                <Typography variant="small" color="blue-gray" className="mb-2 font-medium">
-                                    Start Date
+                                    Title
                                 </Typography>
                                 <Input
-                                    type="datetime-local"
-                                    name="start"
+                                    name="title"
+                                    placeholder="e.g. Breastfeeding Awareness Month"
                                     onChange={formik.handleChange}
                                     onBlur={formik.handleBlur}
-                                    value={formik.values.start}
-                                    error={formik.touched.start && Boolean(formik.errors.start)}
+                                    value={formik.values.title}
+                                    error={formik.touched.title && Boolean(formik.errors.title)}
                                 />
-                                {formik.touched.start && formik.errors.start && (
+                                {formik.touched.title && formik.errors.title && (
                                     <Typography color="red" variant="small">
-                                        {formik.errors.start}
+                                        {formik.errors.title}
                                     </Typography>
                                 )}
                             </div>
-
-                            <div className="w-full">
+                            <div>
                                 <Typography variant="small" color="blue-gray" className="mb-2 font-medium">
-                                    End Date
+                                    Venue
                                 </Typography>
                                 <Input
-                                    type="datetime-local"
-                                    name="end"
+                                    name="venue"
+                                    placeholder="e.g. Taguig City"
                                     onChange={formik.handleChange}
                                     onBlur={formik.handleBlur}
-                                    value={formik.values.end}
-                                    error={formik.touched.end && Boolean(formik.errors.end)}
+                                    value={formik.values.venue}
+                                    error={formik.touched.venue && Boolean(formik.errors.venue)}
                                 />
-                                {formik.touched.end && formik.errors.end && (
+                                {formik.touched.venue && formik.errors.venue && (
                                     <Typography color="red" variant="small">
-                                        {formik.errors.end}
+                                        {formik.errors.venue}
                                     </Typography>
                                 )}
                             </div>
+                            <div>
+                                <Typography variant="small" color="blue-gray" className="mb-2 font-medium">
+                                    Description (Optional)
+                                </Typography>
+                                <Textarea
+                                    name="description"
+                                    placeholder="e.g. Hold a seminar on the importance of breastfeeding."
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.description}
+                                />
+                            </div>
+                            <div className="flex gap-4">
+                                <div className="w-full">
+                                    <Typography variant="small" color="blue-gray" className="mb-2 font-medium">
+                                        Status
+                                    </Typography>
+                                    <Select
+                                        name="status"
+                                        onChange={(val) => formik.setFieldValue("status", val)}
+                                        onBlur={formik.handleBlur}
+                                        value={formik.values.status}
+                                        error={formik.touched.status && Boolean(formik.errors.status)}
+                                    >
+                                        <Option value="Not-Due">Not-Due</Option>
+                                        <Option value="On-Going">On-Going</Option>
+
+                                    </Select>
+                                    {formik.touched.status && formik.errors.status && (
+                                        <Typography color="red" variant="small">
+                                            {formik.errors.status}
+                                        </Typography>
+                                    )}
+                                </div>
+                            </div>
+                            <div className="flex flex-col gap-4">
+                                <div className="w-full">
+                                    <Typography variant="small" color="blue-gray" className="mb-2 font-medium">
+                                        Start Date
+                                    </Typography>
+                                    <Input
+                                        type="datetime-local"
+                                        name="start"
+                                        onChange={formik.handleChange}
+                                        onBlur={formik.handleBlur}
+                                        value={formik.values.start}
+                                        error={formik.touched.start && Boolean(formik.errors.start)}
+                                    />
+                                    {formik.touched.start && formik.errors.start && (
+                                        <Typography color="red" variant="small">
+                                            {formik.errors.start}
+                                        </Typography>
+                                    )}
+                                </div>
+
+                                <div className="w-full">
+                                    <Typography variant="small" color="blue-gray" className="mb-2 font-medium">
+                                        End Date
+                                    </Typography>
+                                    <Input
+                                        type="datetime-local"
+                                        name="end"
+                                        onChange={formik.handleChange}
+                                        onBlur={formik.handleBlur}
+                                        value={formik.values.end}
+                                        error={formik.touched.end && Boolean(formik.errors.end)}
+                                    />
+                                    {formik.touched.end && formik.errors.end && (
+                                        <Typography color="red" variant="small">
+                                            {formik.errors.end}
+                                        </Typography>
+                                    )}
+                                </div>
+                            </div>
                         </div>
-                    </DialogBody>
-                    <DialogFooter>
+                    </div>
+                    <div>
                         <Button type="submit" className="ml-auto">
                             Add Event
                         </Button>
-                    </DialogFooter>
+                    </div>
                 </form>
-            </Dialog>
+            </Drawer>
+
+
+
+
+
+
+
+
         </>
     );
 }
