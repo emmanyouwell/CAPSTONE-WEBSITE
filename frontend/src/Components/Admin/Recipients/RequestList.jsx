da53c9c0c376f3a9cd8b16dee92d4b14e7e72f52
emmanyouwell <emingala02@gmail.com>
2025-05-29 23:17:27 +0800

    Made font size bigger

--- frontend/src/Components/Admin/Recipients/RequestList.jsx
+++ frontend/src/Components/Admin/Recipients/RequestList.jsx
@@ -13,11 +13,11 @@ const RequestList = ({ requests }) => {
                 {requests.map((request, index) => (
                         <li key={index}>
                         <p className="block h-full rounded-lg border border-gray-500 p-4">
-                            <strong className="font-medium text-black">Date: {formatDate(request.date)}</strong>
-                            <p className="mt-1 text-xs font-medium text-gray-800">Requested Volume: {request.volumeRequested.volume} mL/day</p>
-                            <p className="mt-1 text-xs font-medium text-gray-800">Diagnosis: {request.diagnosis}</p>
-                            <p className="mt-1 text-xs font-medium text-gray-800">Reason for request: {request.reason}</p>
-                            <p className="mt-1 text-xs font-medium text-gray-800">Attending Doctor: {request.doctor}</p>
+                            <strong className="font-medium text-lg text-black">Date: {formatDate(request.date)}</strong>
+                            <p className="mt-1 text-lg font-medium text-gray-800">Requested Volume: {request.volumeRequested.volume} mL/day</p>
+                            <p className="mt-1 text-lg font-medium text-gray-800">Diagnosis: {request.diagnosis}</p>
+                            <p className="mt-1 text-lg font-medium text-gray-800">Reason for request: {request.reason}</p>
+                            <p className="mt-1 text-lg font-medium text-gray-800">Attending Doctor: {request.doctor}</p>
                         </p>
                         
                     </li>
